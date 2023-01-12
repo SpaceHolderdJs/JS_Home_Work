5af1e64a702f0016d316891a78db66ff24bf81d2
AlexChehov <0650343005id@gmail.com>
2023-01-12 22:43:14 +0200

    dz1

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// JavaScript Basics
+
+// Task1
+
+const string1 = "100000";
+const string2 = "100000000";
+
+console.log(string1.length);
+console.log(string2.length);
+console.log(string1.length+string2.length);
+
+
+
+// Task2
+
+const sentence1 = "Hello World";
+
+console.log(sentence1);
+console.log(sentence1.replace("Hello World","World Hello"));
+
+
+// Task3
+
+const sentanse3 = "Test string, .., -- string test";
+
+console.log(sentanse3.replace("Test string, .., -- string test","Test string string test"));   
+
+console.log(sentanse3);
+console.log(`${sentanse3.slice(0,11)}${sentanse3.slice(19,31)}`);
+
+
+
+// я за 2 пари зрозумів більше а ніж за місяць до цього )))
+
+
+
+
+
