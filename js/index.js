9ef2ab6a0825e57af320da921a7ce03a3c509d86
AlexChehov <0650343005id@gmail.com>
2023-01-13 11:31:57 +0200

    dz2

--- js/index.js
+++ js/index.js
@@ -11,9 +11,9 @@ if (a > b) {
 
 //2
 
-const year = prompt("Введіть ваш рік народження");
+// const year = prompt("Введіть ваш рік народження");
 
-alert(`Вам ${2023 - year} років!`);
+// alert(`Вам ${2023 - year} років!`);
 
 //3
 
@@ -21,4 +21,28 @@ const name1 = "Igor";
 const name2 = "Oleg";
 const name3 = "Olena";
 const name4 = "Nastya";
+
+
+const nam1 = prompt("Введіть им`я");
+
+switch (nam1) {
+    case ("Igor"):
+        alert("MAN");
+
+        break;
+
+    case ("Oleg"):
+        alert("MAN");
+
+        break;
+
+    case ("Olena"):
+        alert("Woman");
+
+        break;
+
+    case ("Nastya"):
+        alert("Woman");
+
+}
 
