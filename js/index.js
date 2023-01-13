377b4ca4aceddde84691a27ff98255dd79a815a8
AlexChehov <0650343005id@gmail.com>
2023-01-13 10:10:49 +0200

    dz2

---
+++ js/index.js
@@ -0,0 +1,24 @@
+//1
+
+const a = 10;
+const b = 15;
+
+if (a > b) {
+    console.log("FALSE");
+} else if (a < b) {
+    console.log("TRUE")
+}
+
+//2
+
+const year = prompt("Введіть ваш рік народження");
+
+alert(`Вам ${2023 - year} років!`);
+
+//3
+
+const name1 = "Igor";
+const name2 = "Oleg";
+const name3 = "Olena";
+const name4 = "Nastya";
+
