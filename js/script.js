4ff9aae08683093c50ca0291015aeccaba256deb
AlexChehov <0650343005id@gmail.com>
2023-01-20 23:02:19 +0200

    dz4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//1
 
 const user1 = {
 	name: true,
@@ -12,7 +13,24 @@ for (let key in user1 ) {
     delete user2['name']
     delete user2['age']
 }
-
-
 console.log(user1);
 console.log(typeof '', user2);
+console.log(user2.country);
+
+
+//2
+
+let e = {n: 1, m: 2};
+let d = {a: 1, b: 2, c: 3};
+
+let b = {
+    
+};
+
+for (let i = 0 ; i <= String.length; i++){
+    console.log();
+};
+
+
+
+console.log(e);
