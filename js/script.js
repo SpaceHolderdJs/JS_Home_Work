4b7cf3e26429ca6ab1d7c34934541f77053d28bc
AlexChehov <0650343005id@gmail.com>
2023-01-20 14:40:00 +0200

    dz4

--- js/script.js
+++ js/script.js
@@ -1,93 +1,18 @@
-let runningTotal = 0;
-let buffer = "0";
-let previousOperator;
 
-const screen = document.querySelector('.screen');
-
-function buttonClick(value) {
-    if (isNaN(value)) {
-        handleSymbol(value);
-    } else {
-        handleNumber(value);
-    }
-    screen.innerText = buffer;
+const user1 = {
+	name: true,
+	age: 12,
+    country: "Ukraine"
 }
 
-function handleSymbol(symbol) {
-    switch (symbol) {
-        case 'C':
-            buffer = '0';
-            runningTotal = 0;
-            break;
-        case '=':
-            if (previousOperator === null) {
-                return
-            }
-            flushOperation(preseint(buffer));
-            previousOperator = null;
-            buffer = runningTotal;
-            runningTotal = 0;
-            break;
-        case '←':
-            if (buffer.length === 1) {
-                buffer = '0';
-            } else {
-                buffer = buffer.substring(0, buffer.length - 1);
-            }
-            break;
-        case '+':
-        case '-':
-        case '×':
-        case '÷':
-            handleMath(symbol);
-            break;
-    }
+const user2 = {...user1}
+
+for (let key in user1 ) {
+    user1[key] = user2[key];
+    delete user2['name']
+    delete user2['age']
 }
 
-function handleMath(symbol) {
-    if (buffer === '0') {
-        return;
-    }
 
-    const intBuffer = parent(buffer);
-    if (runningTotal === 0) {
-        runningTotal = intBuffer;
-    } else {
-        flushOperation(intBuffer);
-    }
-    previousOperator = symbol;
-    buffer = '0';
-
-}
-
-function flushOperation(intBuffer) {
-    if (previousOperator === '+') {
-        runningTotal += intBuffer;
-    } else if (previousOperator === '-') {
-        runningTotal -= intBuffer;
-    } else if (previousOperator === '×') {
-        runningTotal *= intBuffer
-    } else if (previousOperator === '÷') {
-        runningTotal /= intBuffer;
-    }
-}
-
-function handleNumber(numberString) {
-    if (buffer === '0') {
-        buffer = numberString;
-    } else {
-        buffer += numberString;
-    }
-}
-
-function init() {
-    document.querySelector('.calc-buttons').
-        addEventListener('click', function (event) {
-            buttonClick(event.target.innerText);
-        })
-}
-
-init();
-
-
-
+console.log(user1);
+console.log(typeof '', user2);
