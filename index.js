e19cc3cc53b335850af06a64fefcf7146fbfc1bd
AlexChehov <0650343005id@gmail.com>
2023-01-29 19:57:55 +0200

    dz6

---
+++ index.js
@@ -0,0 +1,35 @@
+//1
+
+const arr = ["1", "2", "4", "7"];
+const res = [];
+
+for (let el of arr) {
+    console.log(arr);
+}
+
+
+
+// Очікуваний результат
+// [1, 2, 4, 7]; 
+
+//2
+
+const arr1 = ["Poland", "Ukraine", "USA"];
+// змінити існуючий масив на масив виду [”P”, “U”, “U”] 
+// (залишити по першій літері з кожного слова)
+
+// arr1.forEach((el) => {
+//     if (el.includes("P")) {
+//         console.log(el);
+//     }else {(el.includes("U")) 
+//         console.log(el);
+//     }
+// });
+
+
+
+//3
+const arr2 = ["Lviv", "Kyiv", "Poltava"];
+// знайти індекс елемента “Kyiv”
+
+console.log(arr2.indexOf("Kyiv"), "index");
