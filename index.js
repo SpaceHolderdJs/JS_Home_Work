695bc89f8a234bb557bfb0495a4588a1170157f3
AlexChehov <113626854+AlexChehov@users.noreply.github.com>
2023-02-04 18:11:25 +0200

    dz8

---
+++ index.js
@@ -0,0 +1,18 @@
+//1
+
+const arr = [1,6,8,21,56];
+
+
+
+//2
+
+const str = "Hello world random words here";
+
+
+
+//3
+
+const arr2 = [20, 90, 7, 8, 100];
+
+let max = arr2[0];
+let min = arr2[0];
