59f4fb7d0a41e2851c42bf56c606228a2319799a
AlexChehov <113626854+AlexChehov@users.noreply.github.com>
2023-02-12 15:52:30 +0200

    dz9

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 //3.Відсортувати масив виду за зростанням та окремо за спаданням
 
-// const arr3 = [1,10, 3, 5, 9];
+const arr3 = [1,10, 3, 5, 9];
 
-// arr3.sort((a, b) => a - b);
-// console.log( arr3 );
+arr3.sort((a, b) => a - b);
+console.log( arr3 );
 
-// arr3.sort((a, b) => b - a);
-// console.log( arr3 );
+arr3.sort((a, b) => b - a);
+console.log( arr3 );
 
 //1 створити масив на базі існуючого виду 
 
-// const arr1 = [[1,2,3], [4,5,6]];
+const arr1 = [[1,2,3], [4,5,6]];
 
-// const res = arr1.map((arr1Element) => {
-//     return arr1Element.map((Element) => String(Element) )
-// })
-// console.log(res); 
+const res = arr1.map((arr1Element) => {
+    return arr1Element.map((Element) => String(Element) )
+})
+console.log(res); 
 
 // [["1", "2", "3"], ["4", "5", "6"]];
 
