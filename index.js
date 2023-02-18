e74aabf80beb4b0c99ff311192337a676dee1bd1
AlexChehov <113626854+AlexChehov@users.noreply.github.com>
2023-02-18 12:18:22 +0200

    dz11

--- index.js
+++ index.js
@@ -2,13 +2,29 @@
 // число аргументом та повертає додатнє число
 //  на базі від`ємного -1 → 1
 
+function key(x) {
+    x = +x; // преобразуем в число
+    if (x === 0 || isNaN(x)) {
+      return x;
+    }
+    return x > 0 ? 1 : -1;
+  }
+console.log(key({}));
+
+function keys() {
+    return (0-1);
+}
+console.log(keys({  }));
 
 
 // 2. Написати функцію що отримує
-//  об`єкт та повертає всі його ключі у вигляді масиву 
+//  об`єкт та повертає всі його ключі у вигляді масиву
 //   {name: “Igor”, age: 23} → [”name”, “age”]
 
-
+function keys (obj) {
+return Object.keys(obj);
+}
+console.log(keys({name:"Alex", age:28}));
 
 
 // Співбесіда!!!!
