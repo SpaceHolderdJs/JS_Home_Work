675f5fd80ca6cd111b5d2e393a0e461d8fc0c6d3
AlexChehov <113626854+AlexChehov@users.noreply.github.com>
2023-03-05 11:37:54 +0200

    dz17

---
+++ index.js
@@ -0,0 +1,51 @@
+const nameInput = document.querySelector("#name");
+const ageInput = document.querySelector("#age");
+const createButton = document.querySelector("#create");
+const userSection = document.querySelector("#users-section");
+
+let users = [];
+
+const deleteUser = (indexofusers) => {
+    users =users.filter((el,i) => i !== indexofusers);
+    renderUsers();
+ };
+
+const renderUsers = () => {
+    userSection.innerHTML = "";
+    const UsersContent = users.map((user) => `<div class="usersCard">
+    <p>${user.name}</p>
+    <span>${user.age}</span>
+    <button class="delete-user-button">Delete</button>
+    </div>`);
+    console.log(UsersContent);
+
+
+    UsersContent.forEach((userLayout) => {
+        userSection.innerHTML += userLayout;
+    });
+    const deleteButtons = [...document.querySelectorAll(".delete-user-button")];
+
+    deleteButtons.forEach((button,index) => {
+        button.onclick = () => deleteUser(index);
+    });
+};
+
+// const deleteUser = () => {
+//     deleteButtons.forEach((button) => {
+//         button.onclick = () => deleteUser();
+//     });
+// };
+
+createButton.onclick = () => {
+    const name = nameInput.value;
+    const age = +ageInput.value;
+
+    const user = { name: name, age: age };
+
+    users.push(user);
+
+    nameInput.value = "";
+    ageInput.value = "";
+
+    renderUsers();
+};
