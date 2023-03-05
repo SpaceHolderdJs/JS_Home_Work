bc4752d6e67d7c3d9e7307b7fb4050b53549a3ac
AlexChehov <113626854+AlexChehov@users.noreply.github.com>
2023-03-05 11:28:03 +0200

    dz15

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 // arr.at(0)                    // повертає {element: 1, array: [1, 2, 3]}
 
 
-
-
 class SuperArray extends Array {
     constructor(el) {
         super(el);
@@ -22,6 +20,21 @@ SuperArray.prototype.copy = function () {
 
 const arr = new Array([1, 2, 3]);
 console.log(arr);
-arr.showInfo();
 
 
+// Написати метод, що приймає назву приватної
+// змінної классу та робить її не приватну копію 
+
+class SuperArray1 extends Array {
+    constructor (arr) {
+        super(arr);
+    }
+    at() {
+        return{
+            al:this[0][this[0].length - 1],
+            array:this[0] 
+        }
+    }
+}
+const array = new SuperArray1([1,2,3,4]);
+console.log(array.at(0));
